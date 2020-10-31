9b2d18cf75650d3c1b66a5b05b974df2264009e4
Evgeniy Rezanov <evgrezanov@gmail.com>
2020-11-01 02:02:33 +0700

    init

---
+++ wp-mybiglife-datafeed-attributes-settings.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: WP Mybiglife Shop Datafeed Atributts
+ * Description: Some settings for woocommerce attributes and datafeed
+ * Plugin URI: https://github.com/evgrezanov/wp-mybiglifeshop-datafeed-attributes
+ * Version: 0.1.0
+ * Author: Evgeniy Rezanov
+ * Author URI: https://github.com/evgrezanov/wp-mybiglifeshop-datafeed-attributes
+ * 
+ */
+class wpMyBigLifeDataFeedAttributesSettings{
+
+    public static function init (){
+        
+    }
+}
+ wpMyBigLifeDataFeedAttributesSettings::init();
+ ?>
